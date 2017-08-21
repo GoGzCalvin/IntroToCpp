9fc49bb81800e411f1ea3e71bc690e3edbcbadb1
GoGzCalvin <gogz.calvin@gmail.com>
2017-08-21 08:39:04 -0700

    dino

---
+++ 4-Functions/Main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int sumOfTwoInts(int a, int b)
+	{
+		return a + b;
+	}
+
+
+
+
+
+
+}
--- HelloWorld/Main.cpp
+++ HelloWorld/Main.cpp
@@ -265,17 +265,22 @@ int main()
 	// Field:       Thai Spiciness Rating
 	// Constraints: Any number from 0 to 5, inclusive of both ends.
 	int thaiSpiceRating = 0;
-	std::cout << "How spicy should your food be?\n";
-	std::cin >> thaiSpiceRating;
+	cout << "How spicy should your food be?\n";
+	cin >> thaiSpiceRating;
 	if (thaiSpiceRating > 5) { thaiSpiceRating = 5; }
 	else if (thaiSpiceRating < 0) { thaiSpiceRating = 0; }
-	std::cout << "You ordered with a spiciness rating of " << thaiSpiceRating << "!\n";
+	cout << "You ordered with a spiciness rating of " << thaiSpiceRating << "!\n";
+
+	// ... 
+
 
 	// Field:       Purchase Order for Cookies
 	// Constraints: Must be ordered in multiples of 23. Only whole numbers.
 	//              Round down to the nearest multiple if not a multiple of 23.
 	//
 	//              If nearest multiple is zero, provide an error message.
+
+
 
 	// Field:       Purchase Order for Sketchbooks (3pk)
 	// Constraints: Must order at least 9 sketchbooks total.
@@ -285,15 +290,141 @@ int main()
 	//              the minimum order count or exceeds the maximum order count,
 	//              provide an error message.
 
+
+	int orderSketch;
+	cout << "How many Sketchbooks are you buying?  ";
+	cin >> orderSketch;
+
+	if (orderSketch > 30)
+	{
+		cout << "You entered ";
+		cout << orderSketch << endl;
+		cout << "We're sorry but your order is too high. " << endl;
+	}
+
+	else if (orderSketch < 9)
+	{
+		cout << "You entered ";
+		cout << orderSketch << endl;
+		cout << "We're sorry but your order is too low. " << endl;
+	}
+
+	else
+	{
+		cout << "You entered ";
+		cout << orderSketch << endl;
+		cout << "Thanks for shopping with us!" << endl;
+	}
+
+	// ... 
+
+
+
 	// Field:       Enable Motion Blur
 	// Constraints: Must be a 'y' or 'n' value.
 	//
 	//              Provide an error message if any other value.
+	char answer;
+	char yes = 'y';
+	char no = 'n';
+
+	cout << "Would you like to enable Motion Blur? [y/n] ";
+	cin >> answer;
+
+	if (answer == yes )
+	{
+		cout << "You answered " << yes << endl;
+		cout << "Motion Blur is now enabled" << endl;
+	}
+	
+	else if (answer == no)
+	{
+		cout << "You answered " << no << endl;
+		cout << "Motion Blur is not enabled" << endl;
+	}
+
+	else
+	{
+		cout << "Error" << endl;
+	}
+
+	// ... 
+
+	//Four Function Disposable Calculator
+	float value1, value2;
+	char operation;
+	char multi = 'x';		
+	char div = '/';
+	char add = '+';
+	char sub = '-';
+
+	cout << "Please input two numbers. ";
+	cin >> value1 >> value2;
+
+	cout << "Multiply, Divide, Add, or Subtract? [+, -, x, /] ";
+	cin >> operation;
+
+	if (operation == 'x')
+	{
+		cout << (value1 * value2) << endl;
+	}
+	
+	else if (operation == '/')
+	{
+		cout << (value1 / value2) << endl;
+	}
+
+	else if (operation == '+')
+	{
+		cout << (value1 + value2) << endl;
+	}
+
+	else if (operation == '-')
+	{
+		cout << (value1 - value2) << endl;
+	}
+
 
 	// ...
 
 
+	// Battle of Prehistoric Proportions
+	int playerScore = 0;
+	int dinoScore = 0;
+	int rateStrength;
+	cout << "How would you rate your strenth on a a scale of 1 to 10? ";
+	cin >> rateStrength;
 
+	if (rateStrength < 4)
+	{
+		cout << "Your strength is no match for the Dino." << endl;
+		dinoScore + 1;
+	}
+
+	else if (rateStrength < 4)
+	{
+		cout << "Your're stronger than the Dino." << endl;
+			playerScore + 1;
+	}
+
+	else if (rateStrength = 4)
+	{
+		cout << "You and the dino equal in strength." << endl;
+		playerScore + 0;
+		dinoScore + 0;
+	}
+	
+	int playerChoice;
+	cout << "Which of the following do you take into battle? \n 1.) A sack of potatoes \n 2.) An invisibility cape \n 3.) The Mango Blade \n";
+	cin >> playerChoice;
+
+	if (playerChoice = 1)
+	{
+		cout << "You've been beaten by the dino's invisibility cape." << endl;
+		dinoScore + 1;
+	}
+
+	else if ()
 	// ... 
 
 
