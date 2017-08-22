34f3b80723c5b474f6c5c6e6d3432077de16f4a2
s179068 <s179068@SEA-E17155>
2017-08-21 17:44:56 -0700

    Multi Files Started

--- 5-Multi-Files/Main.cpp
+++ 5-Multi-Files/Main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
-
+#include "func.h"
 using namespace std;
 
 int main()
 {
+	int numA = 1;
+	int numB = 2;
 
+	cout << "Sum: " << sum(numA, numB) << endl;
+
+	system("pause");
 }
---
+++ 5-Multi-Files/func.h
@@ -0,0 +1,3 @@
+#pragma once
+// Returns the sum of two integers.
+int sum(int a, int b);
